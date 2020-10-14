5e452b3c3ca03b9f1e854bea9c3e3ec471e948d8
yem-gabriel <famosipe2010@gmail.com>
2020-10-14 13:51:00 +0100

    fixed th modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,  {useState, useEffect}  from "react";
 import './App.css'
 import {
   BrowserRouter as Router,
@@ -11,8 +11,23 @@ import Home from "./view/home";
 import Resume from "./view/resume";
 import Myworks from "./view/myworks";
 import Notfound from "./view/Notfound";
+import {
+  Modal,
+  ModalBody,
+  Container,
+} from 'reactstrap';
 
 export default function App() {
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!toggle)
+
+  useEffect(() => {
+    //functioon to startup modal
+    setTimeout(() => {
+      setModal(true)
+    }, 60000);
+    
+  }, [])
   return (
     <Router>
       <div>
@@ -36,6 +51,21 @@ export default function App() {
           </Route>
         </Switch>
       </div>
+      
+      <Modal c isOpen={modal} toggle={toggle}>
+        <span style={{ float: 'right', marginLeft: 'auto', marginRight: '10px' }}>
+          <i onClick={() => setModal(false)} className="fas fa-times"></i>
+        </span>
+        <ModalBody>
+          <Container >
+            <div className="modal-section">
+              <h1>Hello there</h1>
+              <p>If you want me to help you build something like this</p>
+              <a href='https://wa.link/q1iwye' className="boxed-btn3">Message me</a>
+            </div>
+          </Container>
+        </ModalBody>
+      </Modal>
     </Router>
   );
 }
--- src/view/home.tsx
+++ src/view/home.tsx
@@ -6,27 +6,7 @@ import HeroThreeB from '../layers/HeroThreeB';
 import Herotwo from '../layers/herotwo';
 import Homeslider from '../layers/homeslider';
 import INavbar from '../layers/NavBar';
-import {
-  Modal,
-  ModalBody,
-  Container,
-} from 'reactstrap';
 export default function Home() {
-
-  const [modal, setModal] = useState(false);
-  const toggle = () => setModal(!toggle)
-
-  const [modalPics, setModalPics] = useState(false);
-  const togglePics = () => setModal(!togglePics)
-  useEffect(() => {
-    setTimeout(() => {
-      setModal(true)
-    }, 5000);
-    
-    setTimeout(() => {
-      setModalPics(true)
-    }, 10000)
-  }, [])
   
   return (
     <>
@@ -37,37 +17,6 @@ export default function Home() {
       <HeroThreeB/>
       <AboutMe/>
       <Footer/>
-      
-
-      <Modal c isOpen={modal} toggle={toggle}>
-        <span style={{ float: 'right', marginLeft: 'auto', marginRight: '10px' }}>
-          <i onClick={() => setModal(false)} className="fas fa-times"></i>
-        </span>
-        <ModalBody>
-          <Container >
-            <div className="modal-section">
-              <h1>Hello there</h1>
-              <p>If you want me to help you build something like this</p>
-              <a href='https://wa.link/q1iwye' className="boxed-btn3">Message me</a>
-            </div>
-          </Container>
-        </ModalBody>
-      </Modal>
-
-      <Modal isOpen={modalPics} toggle={togglePics}>
-        <span style={{ float: 'right', marginLeft: 'auto', marginRight: '10px' }}>
-          <i onClick={() => setModalPics(false)} className="fas fa-times"></i>
-        </span>
-        <ModalBody>
-          <Container >
-            <div className="modal-section">
-              <h1></h1>
-              <p>Pictures on this website was taken by Tayo Itayemi, message if you like his job</p>
-              <a href='https://wa.link/fm42a7' className="boxed-btn3">Message him</a>
-            </div>
-          </Container>
-        </ModalBody>
-      </Modal>
     </>
   )
 }
